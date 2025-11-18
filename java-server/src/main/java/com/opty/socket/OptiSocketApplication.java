c1c513edbfd0a0d2e5292a4f65d3c2bfdb36a361
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-11-18 11:49:28 -0300

    feat: add spring boot application main class

---
+++ java-server/src/main/java/com/opty/socket/OptiSocketApplication.java
@@ -0,0 +1,33 @@
+/**
+ * Main entry point for Opty Socket Server.
+ * 
+ * @author Opty Development Team
+ * @version 1.1.0
+ */
+
+package com.opty.socket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * CODE
+ */
+
+
+@Slf4j
+@SpringBootApplication
+@EnableScheduling
+@EnableMongoRepositories(basePackages = "com.opty.socket.repository")
+public class OptiSocketApplication {
+
+    public static void main(String[] args) {
+        log.info("Starting Opty Socket Server...");
+        SpringApplication.run(OptiSocketApplication.class, args);
+        log.info("Opty Socket Server started successfully!");
+    }
+}
